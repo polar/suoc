fb87728cca64bc2f088a575269d9897af5cd2cca
Polar Humenn <polar@greene.case.syr.edu>
2009-03-23 20:30:57 -0400

    Trying to get this capistrano to run.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "suoc"
 set :repository,  "git://github.com/polar/suoc.git"
+set :git_enable_submodules, 1
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -13,9 +14,13 @@ set :scm, :git
 
 set :scm_username, "polar"
 
-set :gateway, "suoc.syr.edu:922"
+set :gateway, "polar@adiron.kicks-ass.net:922"
+
 set :user, "suoc"
 
+# WHAT THE FUCK IS THIS???
+set :runner, "suoc"
+
 role :app, "rails.local"
-role :web, "rails.local"
+role :web, "suoc@rails.local:8110"
 role :db,  "rails.local", :primary => true
