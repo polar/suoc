70cc0efdd1897955d8934206c5634ad2b9ea7fc6
Polar Humenn <polar@greene.case.syr.edu>
2009-03-26 10:07:21 -0400

    fix

--- db/migrate/20090326134259_initial_populate.rb
+++ db/migrate/20090326134259_initial_populate.rb
@@ -1,6 +1,6 @@
 class InitialPopulate < ActiveRecord::Migration
   def self.up
-    load(File(RAILS_ROOT+'/db/bootstrap/001_seed.rb'))
+    load(RAILS_ROOT+'/db/bootstrap/001_seed.rb')
   end
 
   def self.down
