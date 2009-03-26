14d7b0109e21f898a0b1e6a8ef065ab8ed47b3fe
Polar Humenn <polar@greene.case.syr.edu>
2009-03-26 11:21:53 -0400

    adding staging

--- config/deploy/staging/deploy.rb
+++ config/deploy/staging/deploy.rb
@@ -1,11 +1,11 @@
-set :application, "suoc"
+set :application, "suoc_staging"
 set :repository,  "git://github.com/polar/suoc.git"
 set :git_enable_submodules, 1
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/suoc_staging"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -18,6 +18,9 @@ set :scm_username, "polar"
 
 set :gateway, "polar@adiron.kicks-ass.net:922"
 set :user, "deploy"
+
+set :rails_env, "staging"
+set :monit_group, "suoc"
 
 # WHAT THE FUCK IS THIS???
 # Why isn't this :app_user, or something logical?
---
+++ config/environments/staging.rb
@@ -0,0 +1,9 @@
+#
+# Staging
+#
+config.cache_classes = true
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching = true
+config.action_view.cache_template_loading
+
+APP_URL = "http://adiron.kicks-ass.net:9200"
