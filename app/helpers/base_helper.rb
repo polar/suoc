4f552eb11061a1929d293e843a77206210e40d9f
Polar Humenn <polar@greene.case.syr.edu>
2009-04-07 00:38:42 -0400

    update

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -4,14 +4,14 @@ module BaseHelper
   # derives "club_member" from a user with the ClubMember extension.
   #
   def club_member_post_url(a,b,args={});user_post_url(a,b,args);end
-  def club_member_friendship_(a,b,args={}); user_friendship_(a,b,args);end
-  def club_member_photo_(a,b,args={}); user_photo_(a,b,args);end
-  def club_member_activity_(a,b,args={}); user_activity_(a,b,args);end
-  def club_member_nessage_(a,b,args={}); user_message_(a,b,args);end
-  def club_member_clipping_(a,b,args={}); user_clipping_(a,b,args);end
-  def club_member_invitation_(a,b,args={}); user_invitation_(a,b,args);end
-  def club_member_offering_(a,b,args={}); user_offering_(a,b,args);end
-  def club_member_favorite_(a,b,args={}); user_favorite_(a,b,args);end
-  def club_member_comment_(a,b,args={}); user_comment_(a,b,args);end
+  def club_member_friendship_url(a,b,args={}); user_friendship_url(a,b,args);end
+  def club_member_photo_url(a,b,args={}); user_photo_url(a,b,args);end
+  def club_member_activity_url(a,b,args={}); user_activity_url(a,b,args);end
+  def club_member_nessage_url(a,b,args={}); user_message_url(a,b,args);end
+  def club_member_clipping_url(a,b,args={}); user_clipping_url(a,b,args);end
+  def club_member_invitation_url(a,b,args={}); user_invitation_url(a,b,args);end
+  def club_member_offering_url(a,b,args={}); user_offering_url(a,b,args);end
+  def club_member_favorite_url(a,b,args={}); user_favorite_url(a,b,args);end
+  def club_member_comment_url(a,b,args={}); user_comment_url(a,b,args);end
 
 end
