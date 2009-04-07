d4e803d8cb53b2cd552cc3abbdceab9c4caa0662
Polar Humenn <polar@greene.case.syr.edu>
2009-04-07 00:15:38 -0400

    Added club_member_\*_url functions to handle polymorphic url.

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -1,11 +1,17 @@
 module BaseHelper
-
-  def commentable_url(comment)
-    if comment.commentable_type != "User" && comment.commentable_type != "ClubMember"
-      polymorphic_url([comment.recipient, comment.commentable])+"#comment_#{comment.id}"
-    else
-      user_url(comment.recipient)+"#comment_#{comment.id}"
-    end
-  end
+  #
+  # These are affectively aliass because the polymorphic_url function
+  # derives "club_member" from a user with the ClubMember extension.
+  #
+  def club_member_post_url(a,b,args={});user_post_url(a,b,args);end
+  def club_member_friendship_(a,b,args={}); user_friendship_(a,b,args);end
+  def club_member_photo_(a,b,args={}); user_photo_(a,b,args);end
+  def club_member_activity_(a,b,args={}); user_activity_(a,b,args);end
+  def club_member_nessage_(a,b,args={}); user_message_(a,b,args);end
+  def club_member_clipping_(a,b,args={}); user_clipping_(a,b,args);end
+  def club_member_invitation_(a,b,args={}); user_invitation_(a,b,args);end
+  def club_member_offering_(a,b,args={}); user_offering_(a,b,args);end
+  def club_member_favorite_(a,b,args={}); user_favorite_(a,b,args);end
+  def club_member_comment_(a,b,args={}); user_comment_(a,b,args);end
 
 end
