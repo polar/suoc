d5725531b97759a153a75b9c28a8d75c54ba3f9f
Polar Humenn <polar@greene.case.syr.edu>
2010-03-30 18:24:28 -0400

    Fixed the bottom ledgers page navigation Added a Category Totals to Account Reports

--- app/controllers/acct_reports_controller.rb
+++ app/controllers/acct_reports_controller.rb
@@ -1,8 +1,8 @@
 class AcctReportsController < BaseController
   layout "club_operations"
-  
+
   filter_access_to :all
-  
+
   def index
     @income_accounts = AcctAccount.all( :order => "name ASC",
                                         :conditions => { :account_type_id => AcctAccountType[:Income] })
@@ -13,23 +13,43 @@ class AcctReportsController < BaseController
     @liability_accounts = AcctAccount.all( :order => "name ASC",
                                          :conditions => { :account_type_id => AcctAccountType[:Liability] })
     @categories = AcctCategory.all( )
-    
+
     @start_date = Date.parse (params[:start_date] ? params[:start_date] : fiscal_year_start_date)
     @end_date = Date.parse (params[:end_date] ? params[:end_date] : fiscal_year_end_date)
-    
+
     @income = calculate(@income_accounts, @categories, @start_date, @end_date)
     @expense = calculate(@expense_accounts, @categories, @start_date, @end_date)
-    
+
+    @types = [AcctAccountType[:Income], AcctAccountType[:Expense]]
+    @category_balances = category_calculate(@types, @categories, @start_date, @end_date)
+
     # We calculate assets and liability up to the specific end date from the Epoch.
     # Doesn't make sense otherwise.
     @asset = calculate(@asset_accounts, @categories, nil, @end_date)
     @liability = calculate(@liability_accounts, @categories, nil, @end_date)
-    
+
     @profit_loss = @income[:balance] + @expense[:balance]
     @net_worth = @asset[:balance] + @liability[:balance]
   end
 
 private
+  def category_calculate(types, categories, start_date = nil, end_date = nil)
+    res = []
+    for c in categories do
+      bal = 0
+      bals = []
+      for t in types do
+        tb = c.account_type_balance(t,start_date, end_date)
+        bals[t.id]= tb
+        bal += tb
+      end
+      if ( (bals.select {|x| x != nil && x != 0}).length > 0 )
+        res << { :category => c, :type_bals => bals, :balance => bal}
+      end
+    end
+    return res
+  end
+
   def calculate(accounts, categories, start_date = nil, end_date = nil)
     res = []
     grandtotal = 0
@@ -43,7 +63,7 @@ private
 	  { :category => c, :balance => subtotal}
 	end
 	grandtotal += accttotal
-	res << { :account => ac, 
+	res << { :account => ac,
 		:category_balances => cats,
 		:balance => accttotal}
       end
