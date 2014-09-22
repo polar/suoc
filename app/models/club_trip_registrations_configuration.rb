29ef7464b2a6f32b264de0cb0c9b049ae93502d7
Polar Humenn <polar@adiron.com>
2014-09-22 13:15:19 -0400

    Added validation for email notification for trip registrations

--- app/models/club_trip_registrations_configuration.rb
+++ app/models/club_trip_registrations_configuration.rb
@@ -1,2 +1,3 @@
 class ClubTripRegistrationsConfiguration < ActiveRecord::Base
+  validates_format_of :notification_email, :with => /\A\s*([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)(,\s*[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)*\s*\Z/
 end
